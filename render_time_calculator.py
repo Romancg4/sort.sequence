01571f2a680b76d0404b1617091eecaf1b891b7e
Romancg4 <67838771+Romancg4@users.noreply.github.com>
2022-10-09 22:35:40 +0300

    Update render_time_calculator.py
    Calculated seconds render time

--- render_time_calculator.py
+++ render_time_calculator.py
@@ -1,14 +1,18 @@
 import os
 
-path = r'E:\_Projects\_git\test_fixsequence'
-files = []
+path = r'L:\VantageScenes\Interior_test2\_render\2022-09-29\150'
 tmp = os.listdir(path)
+files = []
 for t in tmp:
     if os.path.isfile(os.path.join(path,t)):
         files.append(t)
+# size = os.path.getsize(files)
+alltime = [] 
+for s in files:
+    edittime = os.path.getmtime(os.path.join(path, s))
+    alltime.append(edittime)
+sec = alltime[-1] - alltime[0]
+minutes = 0
+hours = 0
 
-timeRender = os.path.getctime(files)
-sizefile = os.path.getsize(files)
-print(timeRender)
-print(sizefile)
-print(files)
+print('Render time sequence:', round(sec), 'seconds')
